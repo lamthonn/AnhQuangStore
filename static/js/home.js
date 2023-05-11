39e49864be956fb0f3395ace0956688327c236af
lamthonn <lamvu180903@gmail.com>
2023-05-11 18:43:10 +0700

    trang chu

---
+++ static/js/home.js
@@ -0,0 +1,30 @@
+let vi_tri = 0;
+const slides = document.getElementsByClassName('slider_item');
+
+function Hien_thi_slide() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.transform = `translateX(-${vi_tri * 100}%)`;
+  }
+}
+
+function next() {
+  if (vi_tri < slides.length - 1) {
+    vi_tri++;
+  } else {
+    vi_tri = 0;
+  }
+  Hien_thi_slide();
+}
+
+function prev() {
+  if (vi_tri > 0) {
+    vi_tri--;
+  } else {
+    vi_tri = slides.length - 1;
+  }
+  Hien_thi_slide();
+}
+
+setInterval(next,3500);
+
+Hien_thi_slide();
