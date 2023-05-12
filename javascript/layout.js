d87ee666d49afebf2895e5da62b26bcb9cc1a173
dinhhai <dinhthihai130903@gmail.com>
2023-05-12 21:48:38 +0700

    hai xinh gai cute tot bung 10 diem

--- javascript/layout.js
+++ javascript/layout.js
@@ -1,6 +1,12 @@
 const diachi = document.querySelector('.dia_chi')
 const list_dh = document.querySelector('.fa-bars')
 const exit = document.querySelector('.fa-xmark')
+const luxe = document.querySelector('.luxe')
+const htkh = document.querySelector('.htkh')
+const lien_ket = document.querySelector('.lien_ket')
+const chinh_sach = document.querySelector('.chinh_sach')
+const dndk = document.querySelector('.fa-user')
+
 let isShow = false;
 
 diachi.addEventListener('click',function (){
@@ -19,4 +25,54 @@ list_dh.addEventListener('click',function(){
 
 exit.addEventListener('click',function() {
     document.querySelector('.mobile_dieu_huong').style.display = 'none';
+})
+
+luxe.addEventListener('click',function() {
+    this.isShow = !this.isShow;
+    if(this.isShow == true){
+        document.querySelector('.luxe_main').style.display = 'block';
+    }
+    else {
+        document.querySelector('.luxe_main').style.display = 'none';
+    } 
+})
+
+htkh.addEventListener('click',function() {
+    this.isShow = !this.isShow;
+    if(this.isShow == true){
+        document.querySelector('.htkh_main').style.display = 'block';
+    }
+    else {
+        document.querySelector('.htkh_main').style.display = 'none';
+    }
+})
+
+lien_ket.addEventListener('click',function() {
+    this.isShow = !this.isShow;
+    if(this.isShow == true){
+        document.querySelector('.list_lk').style.display = 'block';
+    }
+    else {
+        document.querySelector('.list_lk').style.display = 'none';
+    }   
+})
+
+chinh_sach.addEventListener('click',function() {
+    this.isShow = !this.isShow;
+    if(this.isShow == true){
+        document.querySelector('.list_cs').style.display = 'block';
+    }
+    else {
+        document.querySelector('.list_cs').style.display = 'none';
+    }
+})
+
+dndk.addEventListener('click',function() {
+    this.isShow = !this.isShow;
+    if(this.isShow == true){
+        document.querySelector('.user').style.display = 'block';
+    }
+    else {
+        document.querySelector('.user').style.display = 'none';
+    }
 })
